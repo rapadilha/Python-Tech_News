3279536f1b17db1aaf11dc68815ee9f72a5a76c1
Raphael <raphaellfpg@gmail.com>
2022-11-11 15:15:37 -0300

    feat: implemeta a funcao search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    query = {'title': {'$regex': title, '$options': 'i'}}
+    database_result = search_news(query)
+    lista = []
+    for res in database_result:
+        tupla = (res['title'], res['url'])
+        lista.append(tupla)
+
+    return lista
 
 
 # Requisito 7
